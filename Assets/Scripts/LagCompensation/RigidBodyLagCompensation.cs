9f65cb2768bd8c60a641988ea81d283debe86ca6
ArkadiAsaturyan <asaturyanarkadik@gmail.com>
2023-08-03 16:29:48 +0400

    fixed player lag

--- Assets/Scripts/LagCompensation/RigidBodyLagCompensation.cs
+++ Assets/Scripts/LagCompensation/RigidBodyLagCompensation.cs
@@ -5,10 +5,11 @@ public class RigidBodyLagCompensation : MonoBehaviourPun, IPunObservable
 {
     [SerializeField] private Rigidbody2D rigidbody;
 
+    private Vector2 networkPosition;
     private void Awake()
     {
-        PhotonNetwork.SendRate = 30;
-        PhotonNetwork.SerializationRate = 30;
+        PhotonNetwork.SendRate = 60;
+        PhotonNetwork.SerializationRate = 60;
     }
 
     public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
@@ -16,10 +17,27 @@ public class RigidBodyLagCompensation : MonoBehaviourPun, IPunObservable
         if (stream.IsWriting)
         {
             stream.SendNext(rigidbody.rotation);
+            stream.SendNext(rigidbody.velocity);
+            stream.SendNext(rigidbody.position);
         }
         else
         {
             rigidbody.rotation = (float)stream.ReceiveNext();
+            rigidbody.velocity = (Vector2)stream.ReceiveNext();
+            rigidbody.position = (Vector2)stream.ReceiveNext();
+            
+            float lag = Mathf.Abs((float)(PhotonNetwork.Time - info.SentServerTime));
+            rigidbody.position += rigidbody.velocity * lag;
         }
+        
     }
+    
+    // public void FixedUpdate()
+    // {
+    //     if (!photonView.IsMine)
+    //     {
+    //         //rigidbody.position = Vector2.MoveTowards(rigidbody.position, networkPosition, Time.fixedDeltaTime);
+    //         rigidbody.position = Vector2.Lerp(rigidbody.position, networkPosition, Time.fixedDeltaTime);
+    //     }
+    // }
 }
